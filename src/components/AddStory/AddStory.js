354dbaac5af47337f6312a1afe252575c83b7d44
Olga Panfilova <gr-olga@ya.ru>
2022-06-24 16:26:32 +0200

    add popup

--- src/components/AddStory/AddStory.js
+++ src/components/AddStory/AddStory.js
@@ -1,7 +1,7 @@
 import {useState} from "react";
 import {postNewStory} from "../../store/appState/actions";
 import {useDispatch} from "react-redux";
-import {deleteStory} from "../../store/user/actions";
+import Popup from "../Popup/Popup";
 
 export default function AddStory(props) {
     const dispatch = useDispatch()
@@ -9,14 +9,18 @@ export default function AddStory(props) {
     const [name, setName] = useState("");
     const [content, setContent] = useState("");
     const [image, setImage] = useState("");
+    const [success, setSuccess] = useState(false)
     const id = props.id
     console.log(id)
-    console.log(name , content , image)
+    console.log(name, content, image)
     const onFormSubmit = (e) => {
         e.preventDefault();
         dispatch(postNewStory(name, content, image, id))
-    };
 
+    };
+    const togglePopup = () => {
+        setSuccess(!success)
+    }
 
     return (
         <>
@@ -24,6 +28,7 @@ export default function AddStory(props) {
                 <h2>Hey</h2>
                 <button onClick={() => setClickBtn(true)}>Post a cool story bro</button>
             </div>
+            {success && <Popup handleClose={togglePopup} content={"you creat a story"}/>}
             <div>
                 {clickBtn ? (
                         <form onSubmit={onFormSubmit}>
@@ -47,14 +52,14 @@ export default function AddStory(props) {
                             <div>
                                 <label>Image url</label>
                                 <input
-                                     type="text"
+                                    type="text"
                                     value={image}
                                     alt="st"
                                     onChange={(e) => setImage(e.target.value)}
                                 />
-                                <img id="blah" src={image} alt="your image" width={300}/>
+                                {image && <img id="blah" src={image} alt="your image" width={300}/>}
                             </div>
-                            <button type="submit">Post</button>
+                            <button type="submit" onClick={togglePopup}>Post</button>
                             <button onClick={() => setClickBtn(false)}>Discard</button>
                         </form>)
                     :
